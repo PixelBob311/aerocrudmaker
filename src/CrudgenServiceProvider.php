0b912997fdf89d654da27c9abdcb43a2bfb771f9
PixelBob311 <tpatata317@gmail.com>
2023-03-02 19:55:08 +0500

    test

--- src/CrudgenServiceProvider.php
+++ src/CrudgenServiceProvider.php
@@ -4,6 +4,14 @@ use Illuminate\Support\ServiceProvider;
 
 class CrudgenServiceProvider extends ServiceProvider {
 
+	public function boot() {
+		if ($this->app->runningInConsole()) {
+			$this->commands([
+				TestCommand::class,
+			]);
+		}
+	}
+
 	public function register() {
 		$this->commands([
 			TestCommand::class,
