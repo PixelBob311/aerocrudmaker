eff2d23c475c11e0015de42a3e0ee54bcc0b35c8
PixelBob311 <tpatata317@gmail.com>
2023-03-02 18:32:56 +0500

    command reg while registering the package

--- src/CrudgenServiceProvider.php
+++ src/CrudgenServiceProvider.php
@@ -4,14 +4,16 @@ use Illuminate\Support\ServiceProvider as ServiceProviderAlias;
 use \Commands\CrudgenCommand;
 
 class CrudgenServiceProvider extends ServiceProviderAlias {
+
+	public function register() {
+		$this->commands([
+			CrudgenCommand::class,
+		]);
+	}
+
 	public function boot() {
 		$this->publishes([
 			__DIR__ . '/../config/crudgen.php' => config_path('crudgen-config.php'),
 		]);
-		if ($this->app->runningInConsole()) {
-			$this->commands([
-				CrudgenCommand::class,
-			]);
-		}
 	}
 }
