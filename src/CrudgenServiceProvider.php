f22a58f693ff1211e44545a62fed45e8a8629333
PixelBob311 <tpatata317@gmail.com>
2023-03-02 18:21:19 +0500

    command reg while booting

--- src/CrudgenServiceProvider.php
+++ src/CrudgenServiceProvider.php
@@ -1,11 +1,17 @@
 <?php
 
 use Illuminate\Support\ServiceProvider as ServiceProviderAlias;
+use \Commands\CrudgenCommand;
 
 class CrudgenServiceProvider extends ServiceProviderAlias {
 	public function boot() {
 		$this->publishes([
 			__DIR__ . '/../config/crudgen.php' => config_path('crudgen-config.php'),
 		]);
+		if ($this->app->runningInConsole()) {
+			$this->commands([
+				CrudgenCommand::class,
+			]);
+		}
 	}
 }
