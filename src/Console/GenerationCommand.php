5d13bffb3062e2d50c435d72729a4f2b7da4e793
PixelBob311 <tpatata317@gmail.com>
2023-03-02 18:47:30 +0500

    fix

--- src/Console/GenerationCommand.php
+++ src/Console/GenerationCommand.php
@@ -6,10 +6,16 @@ use Illuminate\Console\Command as Command;
 
 class GenerationCommand extends Command {
 	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'crud:generate';
+
+	/**
 	 * Execute the console command.
 	 */
-	public function handle()
-	{
+	public function handle() {
 		dd('hello from my package!!!');
 	}
 }
